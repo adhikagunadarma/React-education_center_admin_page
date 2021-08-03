a166817e9fa876e21db28b25af0bf2915a7ebbfb
Adhika Gunadarma <adhikagunadarma@gmail.com>
2021-08-03 15:37:35 +0700

    update a few from list category

---
+++ src/views/users/CategoriesData.js
@@ -0,0 +1,9 @@
+const categories = [
+    {id: 0, name: 'Chemistry', description: 'Chemistry is a fun language', action: 'Update, Delete'},
+    {id: 1, name: 'Physics', description: 'Physics was definitely not a fun subject', action: 'Update, Delete'},
+    {id: 2, name: 'Biology', description: 'Lets have sex and drink all my cum', action: 'Update, Delete'},
+    {id: 3, name: 'Algorithm', description: 'Algorithm is a fun language', action: 'Update, Delete'},
+  ]
+  
+  export default categories
+  
