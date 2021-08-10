4fbf14993edeb0a32b2750cb3eb909c5ce6bcabe
Adhika Gunadarma <adhikagunadarma@gmail.com>
2021-08-10 11:05:10 +0700

    fetch data from api for list category

--- src/views/pages/category/ListCategory.js
+++ src/views/pages/category/ListCategory.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   CBadge,
   CCard,
@@ -8,15 +8,18 @@ import {
   CDataTable,
   CRow,
   CButton,
-  CLink
+  CLink,
+  CModal,
+  CModalBody,
+  CToaster,
+  CToast,
+  CToastHeader,
+  CToastBody,
+  CLabel
 } from '@coreui/react'
 
 import CIcon from '@coreui/icons-react'
 import { DocsLink } from 'src/reusable'
-
-// import usersData from '../../users/UsersData'
-
-import categories from '../../users/CategoriesData'
 import { Link } from 'react-router-dom'
 
 const getBadge = status => {
@@ -30,12 +33,78 @@ const getBadge = status => {
 }
 const fields = ['name','description', 'action']
 
+
 const ListCategory = () => {
+  
+const [loadingModal, setLoadingModal] = React.useState(false)
+const [categories, setCategories] = React.useState([])
+
+const [position, setPosition] = React.useState('top-center')
+const [autohide, setAutohide] = React.useState(true)
+const [autohideValue, setAutohideValue] = React.useState(5000)
+const [closeButton, setCloseButton] = React.useState(true)
+const [fade, setFade] = React.useState(true)
+const [statusColor , setStatusColor] = React.useState('info')
+const [statusMessage , setStatusMessage] = React.useState('')
+
+const [toasts, setToasts] = React.useState([])
+
+const addToast = () => {
+  setToasts([
+    ...toasts, 
+    { position, autohide: autohide && autohideValue, closeButton, fade, statusMessage, statusColor }
+  ])
+}
+const toasters = (()=>{
+  return toasts.reduce((toasters, toast) => {
+    toasters[toast.position] = toasters[toast.position] || []
+    toasters[toast.position].push(toast)
+    return toasters
+  }, {})
+})()
+
+useEffect(() => {
+  getData()
+  if (statusMessage != ''){
+    addToast() // kalo abis ada perubahan status message / color, baru add tiast
+  }
+}, [statusColor,statusMessage]);
+
+function getData() {
+
+  setLoadingModal(true)
+  return new Promise((resolve) => {
+      const baseEndpoint = "http://localhost:8080"
+      const pathEndpoint = "/api/educen/categories"
+      const requestOptions = {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+      };
+      fetch(baseEndpoint + pathEndpoint)
+          .then(response => response.json())
+          .then(data => {
+            setTimeout((_) => {
+              setLoadingModal(false)
+              if (data.statusCode === 0){
+                resolve(true)
+                setCategories(data.data)
+              }else{
+                resolve(false)
+                setStatusColor('danger')
+                setStatusMessage(data.statusMessage)
+              }
+            },1000)
+   
+          });
+  })
+
+
+}
   return (
     <>
      
       <CRow>
-        <CCol>
+        <CCol sm="12" lg="12">
           <CCard>
             <CCardHeader>
               List Category
@@ -89,7 +158,44 @@ const ListCategory = () => {
             />
             </CCardBody>
           </CCard>
+          <CModal 
+              show={loadingModal} 
+              onClose={setLoadingModal}
+            >
+              <CModalBody>
+                Please wait a moment..
+              </CModalBody>
+            </CModal>
         </CCol>
+        <CCol sm="12" lg="12">
+              {Object.keys(toasters).map((toasterKey) => (
+                <CToaster
+                  position={toasterKey}
+                  key={'toaster' + toasterKey}
+                >
+                  {
+                    toasters[toasterKey].map((toast, key)=>{
+                    return(
+                      <CToast
+                        key={'toast' + key}
+                        show={true}
+                        autohide={toast.autohide}
+                        fade={toast.fade}
+                        color={toast.statusColor}
+                      >
+                        <CToastHeader closeButton={toast.closeButton}>
+                          Alert Notification
+                        </CToastHeader>
+                        <CToastBody>
+                          <CLabel>{toast.statusMessage}</CLabel>
+                        </CToastBody>
+                      </CToast>
+                    )
+                  })
+                  }
+                </CToaster>
+              ))}
+            </CCol>
       </CRow>
     </>
   )
