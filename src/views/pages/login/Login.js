2defd734b998431dcdbfa9b6b8b5f85be8f028da
Adhika Gunadarma <adhikagunadarma@gmail.com>
2021-09-27 22:16:59 +0700

    restructur the code and put the whole http call in the service auth class

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
 
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
+import { useAuth } from 'src/service/auth';
 import {
   CButton,
   CCard,
@@ -29,12 +29,12 @@ import CIcon from '@coreui/icons-react'
 const Login = () => {
 
   const history = useHistory();
-  const [validationError, setValidationError] = React.useState(false)
-
+  const { login } = useAuth()
+  
   const [teacherUsername, setTeacherUsername] = React.useState('')
   const [teacherPassword, setTeacherPassword] = React.useState('')
 
-
+  const [validationError, setValidationError] = React.useState(false)
   const [loadingModal, setLoadingModal] = React.useState(false)
   const [statusColor, setStatusColor] = React.useState('info')
   const [statusMessage, setStatusMessage] = React.useState('')
@@ -62,58 +62,24 @@ const Login = () => {
 
   useEffect(() => {
     if (statusMessage != '') {
-      addToast() // kalo abis ada perubahan status message / color, baru add tiast
-      // setStatusMessage('')
-      // setStatusColor('info')
+      addToast()
     }
   }, [statusColor, statusMessage]);
 
-  function submitData() {
+  async function submitData() {
     if (teacherUsername === '' || teacherPassword === '') {
-      // setStatusColor("warning")
-      // setStatusMessage("Mohon mengisi data yang dibutuhkan terlebih dahulu")
       setValidationError(true)
       return
     }
+
     setLoadingModal(true)
-    return new Promise((resolve) => {
-      const baseEndpoint = "http://localhost:8080"
-      const pathEndpoint = "/api/educen/teacher/login"
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          teacherUsername: teacherUsername,
-          teacherPassword: teacherPassword,
-        })
-      };
-      console.log(JSON.parse(requestOptions.body))
-      fetch(baseEndpoint + pathEndpoint, requestOptions)
-        .then(response => response.json())
-        .then(data => {
-          setTimeout((_) => {
-            setLoadingModal(false)
-            if (data.statusCode === 0) {
-              resolve(true)
-              setStatusColor('success')
-              // need to save the credential information in local to provide data later on when doing task
-              let loginInfo = data.data[0]
-              sessionStorage.setItem('loginInfo', JSON.stringify(loginInfo))
-
-              // also need to genertate token that save the credential for amount of time
-              // need to work on the routing, not login cannot access the page
-              history.push("/dashboard");
-            } else {
-              resolve(false)
-              setStatusColor('danger')
-            }
-            setStatusMessage(data.statusMessage)
-          }, 1000)
-
-        });
-    })
-
-
+    let result = await login({username : teacherUsername, password : teacherPassword})
+    setLoadingModal(false)
+    if (result)
+      history.push("/dashboard");
+    else 
+      setStatusColor('danger')
+      setStatusMessage(result)
   }
   return (
     <div className="c-app c-default-layout flex-row align-items-center">
@@ -167,18 +133,6 @@ const Login = () => {
                   </CModalBody>
                 </CModal>
               </CCard>
-              {/* <CCard className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                      labore et dolore magna aliqua.</p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>Register Now!</CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard> */}
             </CCardGroup>
           </CCol>
 
