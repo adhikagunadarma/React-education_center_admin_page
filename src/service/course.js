43a94ad539bd0ae4f7eab859cfdb8613c23caca5
Adhika Gunadarma <adhikagunadarma@gmail.com>
2021-10-10 13:27:58 +0700

    refactor course service code

--- src/service/course.js
+++ src/service/course.js
@@ -2,62 +2,60 @@
 import { BASE_URL, PATH_URL } from 'src/config.json'
 const axios = require('axios').default;
 
-
-export function useCourseService() {
-    const getCoursesByTeacher = (req) => {
-        return new Promise(async(resolve) => {
-            const baseEndpoint = BASE_URL + PATH_URL
-            const pathEndpoint = "courses/teacher/" + req.id;
-          
-            try {
-              
-                const result = await axios.get(baseEndpoint + pathEndpoint)
-                if (result.status === 200) {
-                    if (result.data.statusCode === 0){
-                        result.data.data.forEach(element => {
-                            
-                            let courseCategory = []
-                            element.courseCategory.map (category => {
-                                    courseCategory.push(category.categoryName)
-                              
-                            })
-                            element.courseCategory = courseCategory
-                        });
-                        
-                        resolve(result.data)
-                    }else{
-                        resolve(result.data)
-                    }
-                } 
-             
-            }
-            catch(e){
-              console.log(e)
-              resolve(e)
-            }
-          })
-}
-
-const getCourse = (req) => {
-return new Promise(async(resolve) => {
-    const baseEndpoint = BASE_URL + PATH_URL
-    const pathEndpoint = "course/" + req.id
-  
-    try {
+export const courseService = {
+  getCoursesByTeacher(req) {
+    return new Promise(async(resolve) => {
+        const baseEndpoint = BASE_URL + PATH_URL
+        const pathEndpoint = "courses/teacher/" + req.id;
       
-        const result = await axios.get(baseEndpoint + pathEndpoint)
-        if (result.data.statusCode === 0 && result.status === 200) {
-          resolve(result.data)
-        } 
-     
-    }
-    catch(e){
-      resolve(e)
-    }
-  })
-}
+        try {
+          
+            const result = await axios.get(baseEndpoint + pathEndpoint)
+            if (result.status === 200) {
+                if (result.data.statusCode === 0){
+                    result.data.data.forEach(element => {
+                        
+                        let courseCategory = []
+                        element.courseCategory.map (category => {
+                                courseCategory.push(category.categoryName)
+                          
+                        })
+                        element.courseCategory = courseCategory
+                    });
+                    
+                    resolve(result.data)
+                }else{
+                    resolve(result.data)
+                }
+            } 
+         
+        }
+        catch(e){
+          console.log(e)
+          resolve(e)
+        }
+      })
+},
 
-const addCourse = (req) => {
+getCourse(req) {
+  return new Promise(async(resolve) => {
+      const baseEndpoint = BASE_URL + PATH_URL
+      const pathEndpoint = "course/" + req.id
+    
+      try {
+        
+          const result = await axios.get(baseEndpoint + pathEndpoint)
+          if (result.data.statusCode === 0 && result.status === 200) {
+            resolve(result.data)
+          } 
+       
+      }
+      catch(e){
+        resolve(e)
+      }
+    })
+  },
+  addCourse(req){
     return new Promise(async(resolve) => {
         const baseEndpoint = BASE_URL + PATH_URL
             const pathEndpoint = "course"
@@ -95,75 +93,64 @@ const addCourse = (req) => {
               resolve(e)
             }
           })
-}
+},
 
-const editCourse = (req) => {
+editCourse (req){
+  return new Promise(async(resolve) => {
+      const baseEndpoint = BASE_URL + PATH_URL
+          const pathEndpoint = "course/" + req.id 
+          const requestBody = {
+                  courseName: req.courseName,
+                  courseDescription: req.courseDescription,
+                  courseThumbnail: req.courseThumbnail,
+                  courseThumbnailName: req.courseThumbnailName,
+                  
+                  courseTrailerFile: req.courseTrailerFile,
+                  courseTrailerName: req.courseTrailerName,
+                  
+                  courseTrailerThumbnailFile: req.courseTrailerThumbnailFile,
+                  courseTrailerThumbnailName: req.courseTrailerThumbnailName,
+                  
+                  courseMembership: req.courseMembership,
+                  coursePublished: req.coursePublished, 
+                  courseTeacher : req.courseTeacher, 
+
+                  courseCategory : req.courseCategory ? req.courseCategory.map((category) => {
+                      if (category.id){
+                          return category.id
+                      }else{
+                          return category._id
+                      }
+                  }) : undefined,
+          }
+          try {
+              const result = await axios.put(baseEndpoint + pathEndpoint, requestBody)
+              if (result.status === 200) {
+                resolve(result.data)
+              } 
+          }
+          catch(e){
+            resolve(e)
+          }
+        })
+  },
+  deleteCourse(req) {
     return new Promise(async(resolve) => {
         const baseEndpoint = BASE_URL + PATH_URL
-            const pathEndpoint = "course/" + req.id 
-            const requestBody = {
-                    courseName: req.courseName,
-                    courseDescription: req.courseDescription,
-                    courseThumbnail: req.courseThumbnail,
-                    courseThumbnailName: req.courseThumbnailName,
-                    
-                    courseTrailerFile: req.courseTrailerFile,
-                    courseTrailerName: req.courseTrailerName,
-                    
-                    courseTrailerThumbnailFile: req.courseTrailerThumbnailFile,
-                    courseTrailerThumbnailName: req.courseTrailerThumbnailName,
-                    
-                    courseMembership: req.courseMembership,
-                    coursePublished: req.coursePublished, 
-                    courseTeacher : req.courseTeacher, 
-
-                    courseCategory : req.courseCategory ? req.courseCategory.map((category) => {
-                        if (category.id){
-                            return category.id
-                        }else{
-                            return category._id
-                        }
-                    }) : undefined,
-            }
-            try {
-                const result = await axios.put(baseEndpoint + pathEndpoint, requestBody)
-                if (result.status === 200) {
-                  resolve(result.data)
-                } 
-            }
-            catch(e){
-              resolve(e)
-            }
-          })
-    }
-
-
-    
-    const deleteCourse = (req) => {
-        return new Promise(async(resolve) => {
-            const baseEndpoint = BASE_URL + PATH_URL
-            const pathEndpoint = "course/" + req.id
+        const pathEndpoint = "course/" + req.id
+      
+        try {
           
-            try {
-              
-                const result = await axios.delete(baseEndpoint + pathEndpoint)
-                if (result.status === 200) {
-                  resolve(result.data)
-                } 
-            }
-            catch(e){
-              console.log(e)
-              resolve(e)
-            }
-          })
-}   
-
-return {
-getCoursesByTeacher,
-addCourse,
-editCourse,
-deleteCourse,
-getCourse
-};
+            const result = await axios.delete(baseEndpoint + pathEndpoint)
+            if (result.status === 200) {
+              resolve(result.data)
+            } 
+        }
+        catch(e){
+          console.log(e)
+          resolve(e)
+        }
+      })
+} 
 
 }
