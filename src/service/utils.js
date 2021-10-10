ebf1e049bb424e31c65bad15f6a0a56646236762
Adhika Gunadarma <adhikagunadarma@gmail.com>
2021-10-10 14:17:18 +0700

    toast service remove remark code

--- src/service/utils.js
+++ src/service/utils.js
@@ -20,28 +20,6 @@ export const fileService = {
       }
 }
 
-export const toastService = {
-    statusColor : 'info',
-    statusMessage : '',
-    position : 'top-center',
-    autohide : true,
-    autohideValue : 5000,
-    closeButton : true,
-    fade : true,
-    toasts : [],
-    addToast(){
-        this.toasts = [
-            ...this.toasts,
-            { position : this.position, 
-                autohide: this.autohide && this.autohideValue, 
-                closeButton : this.closeButton, 
-                fade : this.fade, 
-                statusMessage : this.statusMessage, 
-                statusColor : this.statusColor
-            }
-        ]
-    },
-}
 
 export const ToastComponent = (props) => {
  
@@ -93,54 +71,3 @@ export const LoadingModal = (props) => {
       </CModal>
     )
 }
-
-export function useFileService(){
-    const fileToBase64 = (file) => {
-        return new Promise(resolve => {
-          const reader = new FileReader();
-          reader.readAsDataURL(file);
-          reader.onload = () => resolve(reader.result);
-        })
-    
-      }
-      return {
-        fileToBase64
-       };
-}
-
-export function useToastService() {
-
-    const [statusColor, setStatusColor] = React.useState('info')
-    const [statusMessage, setStatusMessage] = React.useState('')
-    const [position, setPosition] = React.useState('bottom-center')
-    const [toasts, setToasts] = React.useState([])
-
-    const addToast = () => {
-        setToasts([
-            ...toasts,
-            { position, autohide: true && 5000, closeButton : true, fade : true, statusMessage, statusColor }
-        ])
-    }
-    const toasters = (() => {
-        return toasts.reduce((toasters, toast) => {
-            toasters[toast.position] = toasters[toast.position] || []
-            toasters[toast.position].push(toast)
-            return toasters
-        }, {})
-    })
-
-   
- 
-    return {
-     toasts,
-     statusColor,
-     statusMessage,
-     setToasts,
-     setStatusColor,
-     setStatusMessage,
-     setPosition,
-     addToast,
-     toasters,
-    };
- 
- }
